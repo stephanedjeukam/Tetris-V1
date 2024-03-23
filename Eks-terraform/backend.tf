615b77b060242193c2c8974c01d0c5846315055c
stephanedjeukam <87180495+stephanedjeukam@users.noreply.github.com>
2024-03-23 04:05:59 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "stephanedjeukambucket" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-southeast-2"
   }
